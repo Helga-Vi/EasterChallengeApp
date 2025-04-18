c6a54e37f847ceb012ce8fe5ce4839c9352c69cd
Helga-Vi <katarina.jorgensen@gmail.com>
2025-04-18 21:15:48 +0200

    syntaksfeil

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, TouchableOpacity, Animated } from "react-native";
 import HappyEasterBanner from '@/components/HappyEasterBanner';
 import ImagePanel from '@/components/ImagePanel';
-import { playSound } from '@/soundManager';
+import { playSound } from '@/SoundManager';
 
 const Index = () => {
   return (
