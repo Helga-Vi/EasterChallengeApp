aa91c6bcc716ac072b29b858826c944de1c9e326
Helga-Vi <katarina.jorgensen@gmail.com>
2025-04-19 15:46:11 +0200

    endrer måten lyd brukes på

--- SoundManager.js
+++ SoundManager.js
@@ -1,20 +1,46 @@
+import { useState, useEffect } from 'react';
 import * as Audio from 'expo-av';
 
-let sound;
+const SoundManager = () => {
+const [sound, setSound] = useState(null);
 
 export const playSound = async () => {
-  console.log('Playing sound');
+  console.log('Loading sound');
   
   if (!sound) {
+    try {
     sound = await Audio.Sound.createAsync(
-      { uri: 'asset:/sounds/047876_chicken-clucking-68610.mp3' },
-      { shouldPlay: true }
+      require('./assets/sounds/047876_chicken-clucking-6861.mp3')
     );
-  }
-
-  try {
-    await sound.playAsync();
+  setSound(sound);
   } catch (error) {
-    console.error('Failed to play sound:', error);
+    console.error('Error loading sound:', error);
   }
+}
+
+  console.log('Playing Sound');
+  try {
+    await sound.playAsync(); 
+  } catch (error) {
+    console.error('Error playing sound:', error);
+  }
+};  
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound');
+           sound.unloadAsync().then(() => {
+            setSound(null);
+           });
+        }
+      : undefined;
+  }, [sound]);
+
+  return playSound;
 };
+
+export default SoundManager
+
+
+
